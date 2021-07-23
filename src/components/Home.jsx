456de0791d0028464bf127a904cd24225c6d4fa0
techdude101 <techdude1011@gmail.com>
2021-07-23 16:08:17 +0100

    Added an option to enter list ID on Home page if no list exists in local storage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,12 @@ import { useHistory } from "react-router-dom";
 import React, {useEffect} from "react";
 import { nanoid } from 'nanoid';
 import Header from "./Header";
+import "./Form.css";
 
 const Home = () => {
     const key = "shared-todo-list";
     let history = useHistory();
+    let url = "";
 
     useEffect(() => {
         const loadTodosFromLocalStorage = () => {
@@ -25,10 +27,24 @@ const Home = () => {
         history.push(`/list/${newId}`);
     }
 
+    const changeHandler = (e) => {
+      url = e.target.value;
+    }
+
+    const submitHandler = (e) => {
+      e.preventDefault();
+      history.push(`/list/${url}`);
+    }
+
     return (
         <div>
             <Header text="Todo List" />
             <h1 onClick={clickHandler}>Click to create a new list</h1>
+            <h3>or enter/paste list ID below</h3>
+            <form className="form" onSubmit={submitHandler}>
+              <input className="form-input" type="text" placeholder="List ID" onChange={changeHandler} />
+              <input className="form-button" type="submit" value="Go" />
+            </form>
         </div>
     )
 }
