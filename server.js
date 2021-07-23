8dcaef996df8c27fd0a55a3413f4a94f48d46382
techdude101 <techdude1011@gmail.com>
2021-07-23 15:18:17 +0100

    Adding server-side redirect for /list

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ app.use(express.static('build/static/'));
 /* GET React App */
 const publicPath = path.join(__dirname, 'build');
 app.use("/", express.static(publicPath));
+
+app.get("/list/*", (req, res) => {
+    res.redirect("/#" + req.route.path);
+});
+
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname + 'build/index.html'));
 });
+
 
 
 const port = process.env.PORT || 5000;
