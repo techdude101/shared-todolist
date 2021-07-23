ad2851bbd8f8ce7ff4736c59da1331b2655b47ef
techdude101 <techdude1011@gmail.com>
2021-07-23 14:14:34 +0100

    Added catch-all url handler

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('build/static/'));
 
 const publicPath = path.join(__dirname, 'build');
 app.use(express.static(publicPath));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname + 'build/index.html'));
 });
 
